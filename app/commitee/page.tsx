f4c09a16ea129b3f2fac308eb3d7539d2f1ade48
unknown <bandarupallidavid2004@gmail.com>
2024-03-28 15:40:08 +0530

    INCREASE THE WHITE BACKGROUND - BOT

--- app/commitee/page.tsx
+++ app/commitee/page.tsx
@@ -8,31 +8,126 @@ export default function Commitee() {
       </h1>
       <div className="flex justify-between pt-20">
         <div className="bg-[#E7E7E7] h-fit flex flex-col items-center w-[49%] py-[3.5rem]">
-          <p className="text-[#A71E25] text-[22px] inter-custom-bolder ">CHIEF <span className="text-black">PATRON</span></p>
-          <p className="text-black inter-custom-bold pt-5">Er. K. Satyanarayana, <span className="font-light">President, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Smt. K. Siva Kanchana Latha, <span className="font-light">Secretary, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Er. K. L. Havish, <span className="font-light">Vice-President, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Er. K. Raja Harin, <span className="font-light">Vice-President, KLEF</span></p>
+          <p className="text-[#A71E25] text-[22px] inter-custom-bolder ">
+            CHIEF <span className="text-black">PATRON</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Er. K. Satyanarayana,{" "}
+            <span className="open-sans-light">President, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Smt. K. Siva Kanchana Latha,{" "}
+            <span className="open-sans-light">Secretary, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Er. K. L. Havish,{" "}
+            <span className="open-sans-light">Vice-President, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Er. K. Raja Harin,{" "}
+            <span className="open-sans-light">Vice-President, KLEF</span>
+          </p>
         </div>
         <div className="bg-[#E7E7E7] h-fit flex flex-col items-center w-[49%] py-[3.5rem]">
           <p className="text-black text-[22px] inter-custom-bolder ">PATRON</p>
-          <p className="text-black inter-custom-bold pt-5">Dr. K. S. Jagannatha Rao, <span className="font-light">Pro Chancellor, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Dr. A. V. S. Prasad,<span className="font-light"> Pro Vice Chancellor, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Dr. N. Venkataram, <span className="font-light">Pro Vice Chancellor, KLEF</span></p>
-          <p className="text-black inter-custom-bold pt-5">Dr. K. Subba Rao,<span className="font-light">Registrar, KLEF</span></p>
+          <p className="text-black open-sans-bold pt-5">
+            Dr. K. S. Jagannatha Rao,{" "}
+            <span className="open-sans-light">Pro Chancellor, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Dr. A. V. S. Prasad,
+            <span className="open-sans-light"> Pro Vice Chancellor, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Dr. N. Venkataram,{" "}
+            <span className="open-sans-light">Pro Vice Chancellor, KLEF</span>
+          </p>
+          <p className="text-black open-sans-bold pt-5">
+            Dr. K. Subba Rao,
+            <span className="open-sans-light">Registrar, KLEF</span>
+          </p>
         </div>
       </div>
-      <div className="flex justify-between pt-10">
-        <div className="bg-[#E7E7E7] w-[19rem] flex justify-center"><p className="text-[#A71E25]">CONFERENCE <span className="text-black">CHAIR</span></p>
-          <p className="text-black">Dr.K. Subramanyam
-            Principal, College of Sciences, KLEF</p>
-        </div>
-        <div className="bg-[#E7E7E7] w-[19rem] flex justify-center"><p className="text-[#A71E25]">CONFERENCE <span className="text-black">CO-CHAIR</span></p>
-        <p className="text-black">Dr.K. Subramanyam
-          Principal, College of Sciences, KLEF</p>
+
+      <div className="flex justify-between mt-10">
+        <div className="bg-[#E7E7E7] w-[19rem] flex flex-col items-center py-10 ">
+          <p className="text-[#A71E25] inter-custom-bolder text-xl">
+            CONFERENCE <span className="text-black">CHAIR</span>
+          </p>
+          <div className="flex flex-col items-center pt-6">
+            <p className="text-black open-sans-boldl">Dr.K. Subramanyam</p>
+            <p className="text-black open-sans-light">
+              Principal, College of Sciences, KLEF
+            </p>
           </div>
-        <div className="bg-[#E7E7E7] w-[19rem] flex justify-center"><p className="text-black">CONVENER</p><p className="text-black">Dr.K. Subramanyam
-          Principal, College of Sciences, KLEF</p></div>
+        </div>
+
+        <div className="bg-[#E7E7E7] w-[19rem] flex flex-col items-center py-10">
+          <p className="text-[#A71E25] inter-custom-bolder  text-xl">
+            CONFERENCE <span className="text-black">CHAIR</span>
+          </p>
+          <div className="flex flex-col items-center pt-6">
+            <p className="text-black open-sans-boldl">Dr.G.Krishna Mohan</p>
+            <p className="text-black open-sans-light">
+              Principal, College of Sciences, KLEF
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-[#E7E7E7] w-[19rem] flex flex-col items-center py-10">
+          <p className="text-[#A71E25] inter-custom-bolder  text-xl">
+            CONFERENCE <span className="text-black">CHAIR</span>
+          </p>
+          <div className="flex flex-col items-center pt-6">
+            <p className="text-black open-sans-boldl">Dr.K. Subramanyam</p>
+            <p className="text-black open-sans-light">
+              Principal, College of Sciences, KLEF
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#E7E7E7] h-fit mt-10 px-10 py-10">
+        <div className="flex justify-center">
+          <p className="text-[#A71E25] inter-custom-bolder text-xl pb-5">
+            STEERING <span className="text-black">COMMITTEE</span>
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-black open-sans-bold text-center">
+            Mr. J A Jevin Assistant <br />
+            <span className="open-sans-regular">
+              Professor, Dept. of CSA, KLEF
+            </span>
+          </p>
+          <p className="text-black open-sans-bold text-center pl-12">
+            Dr. Rama Krishna Yellapragada <br />
+            <span className="open-sans-regular">
+              Assistant Professor, Dept. of CSA, KLEF
+            </span>
+          </p>
+          <p className="text-black open-sans-bold text-center">
+            Dr.B. Krishna Prasad <br />
+            <span className="open-sans-regular">
+              Assistant Professor, Dept. of CSA, KLEF
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="">
+        <div>
+          <h1 className="text-[#A71E25]">
+            ORGANIZING <span className="text-black">COMMITTEE</span>
+          </h1>
+          <p className="text-black">
+            Dr. G. Venkateswarlu, Assistant Professor, Dept. of CSA, KLEF Mr.
+            N.Venkata Ramana, Assistant Professor, Dept. of CSA, KLEF Mr.
+            R.Nagendra Babu, Assistant Professor, Dept. of CSA, KLEF Dr. Gufran
+            Ahmad, Assistant Professor, Dept. of CSA, KLEF Mrs. T.Ratna Kumari,
+            Assistant Professor, Dept. of CSA, KLEF
+          </p>
+        </div>
       </div>
     </div>
   );
