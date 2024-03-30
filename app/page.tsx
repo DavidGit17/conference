2ac1f2231da830be73dbc5ee25a67b99312dcd80
Kiran-Alex <kiranalexc@gmail.com>
2024-03-31 00:45:35 +0530

    minor text fix

--- app/page.tsx
+++ app/page.tsx
@@ -50,17 +50,17 @@ export default function Home() {
 						<div className=" flex flex-col w-full lg:h-64 h-48 justify-evenly items-center lg:pt-0 pt-8 lg:gap-0 gap-3 ">
 							<div className="lg:h-12 lg:w-80 bg-white  rounded-lg flex flex-row text-black justify-center items-center font-semibold lg:text-lg lg:px-4 text-xs px-3 py-2 uppercase font-sans">Conference Mode :<span className="ml-2  text-[#A71E25] font-sans font-black">  Hybrid</span></div>
 							<div className="lg:h-24 h-16 lg:w-11/12 w-full text-white bg-black lg:text-base text-[9px] flex lg:justify-around justify-between items-center lg:px-2 px-2 ">
-								<div className="flex flex-col justify-center items-center font-sans uppercase tracking-wider text-center font-[8px] ">
+								<div className="flex flex-col justify-center items-center font-sans uppercase tracking-wider text-center font-[6px] ">
 									<p className="font-[400] ">Last Date of Registration & </p>
 									<p className="font-[400]">Abstract Submission</p>
 									<p className="font-[700] font-black">5th  April, 2024</p>
 								</div>
 
-								<div className="flex flex-col justify-center items-center font-sans uppercase text-center font-[8px]">
+								<div className="flex flex-col justify-center items-center font-sans uppercase text-center font-[6px]">
 									<p className="font-[400]">Full Paper Submission</p>
 									<p className="font-[700] font-black">10th April 2024</p>
 								</div>
-								<div className="flex flex-col justify-center items-center font-sans uppercase text-center font-[8px]">
+								<div className="flex flex-col justify-center items-center font-sans uppercase text-center font-[6px]">
 									<p className="font-[400]">Conference Date</p>
 									<p className="font-[700] font-black">27th April 2024</p>
 								</div>
@@ -68,7 +68,7 @@ export default function Home() {
 						</div>
 					</div>
 				</div>
-				
+
 
 			</div>
 
